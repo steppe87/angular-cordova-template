5d4855592b8dc34d5d8fc1b224cf95c1f9c3c25c
Stephan Müller <sm@schramlsoft.de>
2021-03-09 22:50:39 +0100

    chore: adding animations

--- template_src/src/app/app.module.ts
+++ template_src/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,9 +13,9 @@ import { MatIconModule } from '@angular/material/icon';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    BrowserModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
